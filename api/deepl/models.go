436e34fe07583d5b7bcf33d6f80c6a289bb5ac39
YouheiKajiro <te0801068@gmail.com>
2023-08-19 15:24:40 +0900

    Refactor DeeplClient for modularity and readability
    - Moved the deeplAPIEndpoint constant to the package level.
    - Broke down the Translate function into smaller, more focused methods (marshalRequest, createRequest, sendRequest, and parseResponse).
    - Updated error handling to be more consistent across methods.

--- api/deepl/models.go
+++ api/deepl/models.go
@@ -11,6 +11,8 @@ import (
 	"github.com/youheiyouhei/transgo/translator"
 )
 
+const deeplAPIEndpoint = "https://api-free.deepl.com/v2/translate"
+
 type DeeplClient struct{}
 
 func NewDeeplClient() *DeeplClient {
@@ -18,38 +20,62 @@ func NewDeeplClient() *DeeplClient {
 }
 
 func (d *DeeplClient) Translate(request translator.TranslationRequest) (string, error) {
-	const deeplAPIEndpoint = "https://api-free.deepl.com/v2/translate"
+	data, err := d.marshalRequest(request)
+	if err != nil {
+		return "", err
+	}
+
+	req, err := d.createRequest(data)
+	if err != nil {
+		return "", err
+	}
+
+	respBody, err := d.sendRequest(req)
+	if err != nil {
+		return "", err
+	}
+
+	return d.parseResponse(respBody)
+}
+
+func (d *DeeplClient) marshalRequest(request translator.TranslationRequest) ([]byte, error) {
 	data, err := json.Marshal(request)
 	if err != nil {
-		return "", fmt.Errorf("could not marshal request data: %v", err)
+		return nil, fmt.Errorf("could not marshal request data: %v", err)
 	}
+	return data, nil
+}
 
+func (d *DeeplClient) createRequest(data []byte) (*http.Request, error) {
 	req, err := http.NewRequest("POST", deeplAPIEndpoint, bytes.NewBuffer(data))
 	if err != nil {
-		return "", fmt.Errorf("could not create new request: %v", err)
+		return nil, fmt.Errorf("could not create new request: %v", err)
 	}
+
 	apiKey, err := config.GetAPIKey()
 	if err != nil {
-		return "", fmt.Errorf("could not get API key: %v", err)
+		return nil, fmt.Errorf("could not get API key: %v", err)
 	}
+
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Authorization", "DeepL-Auth-Key "+apiKey)
+	return req, nil
+}
 
+func (d *DeeplClient) sendRequest(req *http.Request) ([]byte, error) {
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return "", fmt.Errorf("could not make request to DeepL: %v", err)
+		return nil, fmt.Errorf("could not make request to DeepL: %v", err)
 	}
 	defer resp.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", fmt.Errorf("could not read response body: %v", err)
-	}
+	return io.ReadAll(resp.Body)
+}
 
-	var deeplResp *translator.TranslationResponse
-	err = json.Unmarshal(body, &deeplResp)
-	if err != nil {
+func (d *DeeplClient) parseResponse(body []byte) (string, error) {
+	var deeplResp translator.TranslationResponse
+	if err := json.Unmarshal(body, &deeplResp); err != nil {
 		return "", fmt.Errorf("could not unmarshal response data: %v", err)
 	}
 
